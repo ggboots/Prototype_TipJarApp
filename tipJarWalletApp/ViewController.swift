846f8c57a70f5ab6af5cf32ad81d5829ca28432a
George Boots <ggboots.99@gmail.com>
2020-07-24 21:56:48 +1000

    Initial Commit

---
+++ tipJarWalletApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipJarWalletApp
+//
+//  Created by George Boots on 24/7/20.
+//  Copyright © 2020 George Boots. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
