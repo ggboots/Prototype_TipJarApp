b201cc8c852aa260a79f6bd558b238570e673516
George Boots <ggboots.99@gmail.com>
2020-07-27 21:54:04 +1000

    - Created 2  Child Containers on page 1

---
+++ tipJarWalletApp/FirstChildVC.swift
@@ -0,0 +1,16 @@
+//
+//  FirstChildVC.swift
+//  tipJarWalletApp
+//
+//  Created by George Boots on 27/7/20.
+//  Copyright © 2020 George Boots. All rights reserved.
+//
+
+import UIKit
+
+class FirstChildVC: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
